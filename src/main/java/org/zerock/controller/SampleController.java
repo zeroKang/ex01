a84669ce83a298eb615d5ac947343384ece3d46d
zerockcode <zerockcode@gmail.com>
2018-02-15 00:29:53 +0900

    ex01 Notebook Start...

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,12 @@
+package org.zerock.controller;
+
+import lombok.ToString;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+
+}
